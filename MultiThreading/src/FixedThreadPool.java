83007e3c939ba83d36744be3d8afc1ed31263daa
Prigoistic <priyamghosh9753@gmail.com>
2025-06-22 04:12:46 +0530

    Updated fixed pool code

--- MultiThreading/src/FixedThreadPool.java
+++ MultiThreading/src/FixedThreadPool.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FixedThreadPool {
+    public static void main(String[] args) {
+        try (ExecutorService executorService = Executors.newFixedThreadPool(2)) {
+            for (int i = 0; i < 7; i++) {
+                executorService.execute(new Work(i));
+            }
+        }
+    }
+}
+
+class Work implements Runnable{
+
+    private final int workid;
+
+    public Work(int workid) {
+        this.workid = workid;
+    }
+
+    @Override
+
+    public void run(){
+        System.out.println("Task ID " + workid + " being executed by thread " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
